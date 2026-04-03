756e2879f957eb2d06f9de2db759bee7ab3018ec
Sona Binu <sonavbinu567@gmail.com>
2026-04-03 22:55:29 +0530

    add

--- Day2/twoSum.js
+++ Day2/twoSum.js
@@ -1,18 +1,32 @@
+// function twoSum(arr, target) {
+//   let left = 0;
+//   let right = arr.length - 1;
+
+//   while (left < right) {
+//     let sum = arr[left] + arr[right];
+
+//     if (sum === target) {
+//       return [left, right];
+//     } else if (sum < target) {
+//       left++;
+//     } else {
+//       right--;
+//     }
+//   }
+// }
+// console.log(twoSum([1, 3, 4, 5, 6, 7, 8], 10)); // [0, 6]
+// console.log(twoSum([1, 3, 4, 5, 6, 7, 8], 11)); // [1, 6]
+
 function twoSum(arr, target) {
-  let left = 0;
-  let right = arr.length - 1;
+  const map = new Map();
 
-  while (left < right) {
-    let sum = arr[left] + arr[right];
+  for (i = 0; i < arr.lengh; i++) {
+    const x = target - arr[i];
 
-    if (sum === target) {
-      return [left, right];
-    } else if (sum < target) {
-      left++;
-    } else {
-      right--;
+    if (map.has(x)) {
+      return [map.get(x), i];
     }
+
+    map.set(arr[i], i);
   }
 }
-console.log(twoSum([1, 3, 4, 5, 6, 7, 8], 9)); // [0, 6]
-console.log(twoSum([1, 3, 4, 5, 6, 7, 8], 11)); // [1, 6]
